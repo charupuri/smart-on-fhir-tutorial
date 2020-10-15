e1caf08d35949234582ea58e0be987b7867893c2
charupuri <72887291+charupuri@users.noreply.github.com>
2020-10-15 18:02:23 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -27,7 +27,7 @@
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
-          var identi = patient.identifier;
+          var identi = 'gggg';
           var fname = '';
           var lname = '';
 
